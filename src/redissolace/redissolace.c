1b9db2e76f66a4dd7b473d8e8a16fde60b5cb701
Alexey Alexeev <voodoo144@gmail.com>
2021-08-27 21:04:07 -0400

    All logic in one file

--- src/redissolace/redissolace.c
+++ src/redissolace/redissolace.c
@@ -6,21 +6,17 @@
 #include "../solclient/solClient.h"
 #include "../solclient/solClientMsg.h"
 
-/* Message Count */
-static int msgCount = 0;
-
 /*****************************************************************************
  * sessionMessageReceiveCallback
  *
  * The message callback is invoked for each Direct message received by
  * the Session. In this sample, the message is printed to the screen.
  *****************************************************************************/
-solClient_rxMsgCallback_returnCode_t sessionMessageReceiveCallback ( solClient_opaqueSession_pt opaqueSession_p, solClient_opaqueMsg_pt msg_p, void *user_p ){
-    printf ( "Received message:\n" );
-    solClient_msg_dump ( msg_p, NULL, 0 );
-    printf ( "\n" );
-
-    msgCount++;
+solClient_rxMsgCallback_returnCode_t sessionMessageReceiveCallback(solClient_opaqueSession_pt opaqueSession_p, solClient_opaqueMsg_pt msg_p, void *user_p)
+{
+    printf("Received message:\n");
+    solClient_msg_dump(msg_p, NULL, 0);
+    printf("\n");
 
     return SOLCLIENT_CALLBACK_OK;
 }
@@ -30,19 +26,22 @@ solClient_rxMsgCallback_returnCode_t sessionMessageReceiveCallback ( solClient_o
  *
  * The event callback function is mandatory for session creation.
  *****************************************************************************/
-void sessionEventCallback ( solClient_opaqueSession_pt opaqueSession_p,
-                solClient_session_eventCallbackInfo_pt eventInfo_p, void *user_p ){
-
+void sessionEventCallback(solClient_opaqueSession_pt opaqueSession_p,
+                          solClient_session_eventCallbackInfo_pt eventInfo_p, void *user_p)
+{
 }
 
-solClient_returnCode_t SolaceInit(){
-    if(solClient_initialize ( SOLCLIENT_LOG_DEFAULT_FILTER, NULL )==SOLCLIENT_FAIL){
+solClient_returnCode_t SolaceInit()
+{
+    if (solClient_initialize(SOLCLIENT_LOG_DEFAULT_FILTER, NULL) == SOLCLIENT_FAIL)
+    {
         return SOLCLIENT_FAIL;
     }
-    if(solClient_context_create ( SOLCLIENT_CONTEXT_PROPS_DEFAULT_WITH_CREATE_THREAD,
-                               &context_p, &contextFuncInfo, sizeof ( contextFuncInfo ) )==SOLCLIENT_FAIL){
-                                    return SOLCLIENT_FAIL;
-                               }
+    if (solClient_context_create(SOLCLIENT_CONTEXT_PROPS_DEFAULT_WITH_CREATE_THREAD,
+                                 &context_p, &contextFuncInfo, sizeof(contextFuncInfo)) == SOLCLIENT_FAIL)
+    {
+        return SOLCLIENT_FAIL;
+    }
     sessionFuncInfo.rxMsgInfo.callback_p = sessionMessageReceiveCallback;
     sessionFuncInfo.rxMsgInfo.user_p = NULL;
     sessionFuncInfo.eventInfo.callback_p = sessionEventCallback;
@@ -51,77 +50,103 @@ solClient_returnCode_t SolaceInit(){
 }
 
 /* Configure the Session properties. */
-solClient_returnCode_t SolaceConfigure(char* sessionProps[] ){
-    return solClient_session_create ( ( char ** ) sessionProps,
-                               context_p,
-                               &session_p, &sessionFuncInfo, sizeof ( sessionFuncInfo ) );
+solClient_returnCode_t SolaceConfigure(char *sessionProps[])
+{
+    return solClient_session_create((char **)sessionProps,
+                                    context_p,
+                                    &session_p, &sessionFuncInfo, sizeof(sessionFuncInfo));
 }
 
-solClient_returnCode_t SolaceDisconnect(){
-    return solClient_cleanup (  );
+solClient_returnCode_t SolaceDisconnect()
+{
+    return solClient_cleanup();
 }
 
-solClient_returnCode_t SolaceUnsubscribe(char* topicName){
-     return solClient_session_topicUnsubscribeExt ( session_p,
-                                            SOLCLIENT_SUBSCRIBE_FLAGS_WAITFORCONFIRM,
-                                            topicName );
+solClient_returnCode_t SolaceUnsubscribe(char *topicName)
+{
+    return solClient_session_topicUnsubscribeExt(session_p,
+                                                 SOLCLIENT_SUBSCRIBE_FLAGS_WAITFORCONFIRM,
+                                                 topicName);
 }
 
-solClient_returnCode_t SolaceConnect(){
-    return solClient_session_connect ( session_p );
+solClient_returnCode_t SolaceConnect()
+{
+    return solClient_session_connect(session_p);
 }
 
-
-solClient_returnCode_t SolaceSubscribe(char* topicName){
-    return solClient_session_topicSubscribeExt ( session_p,
-                                          SOLCLIENT_SUBSCRIBE_FLAGS_WAITFORCONFIRM,
-                                          topicName );
+solClient_returnCode_t SolaceSubscribe(char *topicName)
+{
+    return solClient_session_topicSubscribeExt(session_p,
+                                               SOLCLIENT_SUBSCRIBE_FLAGS_WAITFORCONFIRM,
+                                               topicName);
 }
 
-int SolaceConfigureCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc){
+int SolaceConfigureCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc)
+{
+    SolaceConfigure();
     RedisModule_ReplyWithSimpleString(ctx, "PASS");
     return REDISMODULE_OK;
 }
 
-int SolaceConnectCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc){
+int SolaceConnectCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc)
+{
+    SolaceConnect();
     RedisModule_ReplyWithSimpleString(ctx, "PASS");
     return REDISMODULE_OK;
 }
 
-int SolaceDisconnectCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc){
+int SolaceDisconnectCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc)
+{
+    SolaceDisconnect();
     RedisModule_ReplyWithSimpleString(ctx, "PASS");
     return REDISMODULE_OK;
 }
 
-int SolaceSendCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc){
+int SolaceSendCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc)
+{
+    SolaceSend();
     RedisModule_ReplyWithSimpleString(ctx, "PASS");
     return REDISMODULE_OK;
 }
 
-int SolaceSubscribeCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc){
+int SolaceSubscribeCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc)
+{
+    SolaceSubscribe();
+    RedisModule_ReplyWithSimpleString(ctx, "PASS");
+    return REDISMODULE_OK;
+}
+
+int SolaceUnsubscribeCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc)
+{
+    SolaceUnsubscribe();
     RedisModule_ReplyWithSimpleString(ctx, "PASS");
     return REDISMODULE_OK;
 }
 
 //Main method to load module
-int RedisModule_OnLoad(RedisModuleCtx *ctx) {
+int RedisModule_OnLoad(RedisModuleCtx *ctx)
+{
 
-  // Register the module itself
-  if (RedisModule_Init(ctx, REDIS_SOLACE_MODULE_NAME, REDIS_SOLACE_MODULE_VERSION, REDISMODULE_APIVER_1) ==
-      REDISMODULE_ERR) {
-    return REDISMODULE_ERR;
-  }
+    // Register the module itself
+    if (RedisModule_Init(ctx, REDIS_SOLACE_MODULE_NAME, REDIS_SOLACE_MODULE_VERSION, REDISMODULE_APIVER_1) ==
+        REDISMODULE_ERR)
+    {
+        return REDISMODULE_ERR;
+    }
+    //init solace
+    SolaceInit();
 
-  // Solace configuration  
-  RMUtil_RegisterReadCmd(ctx, SOLACE_CONFIGURE_COMMAND, SolaceConfigureCommand);
-  // Solace connect 
-  RMUtil_RegisterReadCmd(ctx, SOLACE_CONNECT_COMMAND, SolaceConnectCommand);
-  // Solace disconnect
-  RMUtil_RegisterReadCmd(ctx, SOLACE_DISCONNECT_COMMAND, SolaceDisconnectCommand);
-  // Solace send
-  RMUtil_RegisterReadCmd(ctx, SOLACE_SEND_COMMAND, SolaceSendCommand);
-  // Solace subscribe
-  RMUtil_RegisterWriteCmd(ctx, SOLACE_SUBSCRIBE_COMMAND, SolaceSubscribeCommand);
-
-  return REDISMODULE_OK;
+    // Solace configuration
+    RMUtil_RegisterReadCmd(ctx, SOLACE_CONFIGURE_COMMAND, SolaceConfigureCommand);
+    // Solace connect
+    RMUtil_RegisterReadCmd(ctx, SOLACE_CONNECT_COMMAND, SolaceConnectCommand);
+    // Solace disconnect
+    RMUtil_RegisterReadCmd(ctx, SOLACE_DISCONNECT_COMMAND, SolaceDisconnectCommand);
+    // Solace send
+    RMUtil_RegisterReadCmd(ctx, SOLACE_SEND_COMMAND, SolaceSendCommand);
+    // Solace subscribe
+    RMUtil_RegisterWriteCmd(ctx, SOLACE_SUBSCRIBE_COMMAND, SolaceSubscribeCommand);
+    // Solace unsubscribe
+    RMUtil_RegisterWriteCmd(ctx, SOLACE_UNSUBSCRIBE_COMMAND, SolaceUnsubscribeCommand);
+    return REDISMODULE_OK;
 }
--- src/redissolace/redissolace.h
+++ src/redissolace/redissolace.h
@@ -9,33 +9,37 @@ solClient_context_createFuncInfo_t contextFuncInfo = SOLCLIENT_CONTEXT_CREATEFUN
 solClient_opaqueSession_pt session_p;
 solClient_session_createFuncInfo_t sessionFuncInfo = SOLCLIENT_SESSION_CREATEFUNC_INITIALIZER;
 
-
 solClient_returnCode_t SolaceInit();
 
 solClient_returnCode_t SolaceConnect();
 
-solClient_returnCode_t SolaceConfigure(char* sessionProps[] );
+solClient_returnCode_t SolaceConfigure(char *sessionProps[]);
 
-solClient_returnCode_t SolaceSubscribe(char* topicName);
+solClient_returnCode_t SolaceSubscribe(char *topicName);
 
-solClient_returnCode_t SolaceUnsubscribe(char* topicName);
+solClient_returnCode_t SolaceSend(char *topicName, char* message);
+
+solClient_returnCode_t SolaceUnsubscribe(char *topicName);
 
 solClient_returnCode_t SolaceDisconnect();
 
-const char* REDIS_SOLACE_MODULE_NAME="RedisSolace";
-const int REDIS_SOLACE_MODULE_VERSION=1;
+const char *REDIS_SOLACE_MODULE_NAME = "RedisSolace";
+const int REDIS_SOLACE_MODULE_VERSION = 1;
 
-const char* SOLACE_CONFIGURE_COMMAND="solace.configure";
+const char *SOLACE_CONFIGURE_COMMAND = "solace.configure";
 int SolaceConfigureCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc);
 
-const char* SOLACE_CONNECT_COMMAND="solace.connect";
+const char *SOLACE_CONNECT_COMMAND = "solace.connect";
 int SolaceConnectCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc);
 
-const char* SOLACE_DISCONNECT_COMMAND="solace.disconnect";
+const char *SOLACE_DISCONNECT_COMMAND = "solace.disconnect";
 int SolaceDisconnectCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc);
 
-const char* SOLACE_SUBSCRIBE_COMMAND="solace.subscribe";
+const char *SOLACE_SUBSCRIBE_COMMAND = "solace.subscribe";
 int SolaceSubscribeCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc);
 
-const char* SOLACE_SEND_COMMAND="solace.send";
+const char *SOLACE_UNSUBSCRIBE_COMMAND = "solace.subscribe";
+int SolaceUnsubscribeCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc);
+
+const char *SOLACE_SEND_COMMAND = "solace.send";
 int SolaceSendCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc);
